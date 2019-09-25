798bd099c296b173e3f9b615c8090f7c1ef4af31
SantiagoHerreG <888@holbertonschool.com>
2019-09-25 17:25:19 +0000

    Fixed: push 0 error, betty in pint

--- pint.c
+++ pint.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-/*
+/**
  * pint - prints the value at the top of the stack
  * @stack: double pointer to the first node
  * @line_number: line number in the file where the opcode is located
@@ -8,14 +8,8 @@
 */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp;
-
 	if (*stack == NULL)
 		error_handling("pint", line_number + 1);
 	else
-	{
-		tmp = *stack;
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-	}
+		printf("%d\n", (*stack)->n);
 }
